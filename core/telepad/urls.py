64671b330772c9d5294555c4744ec58b3b834467
ivirare <sintervx@gmail.com>
2025-10-19 20:13:45 +0300

    Bot API url fix

--- core/telepad/urls.py
+++ core/telepad/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     # Mounted at root; Nginx strips /api/ prefix before proxying
     path("", include(("users.urls", "users"), namespace="users")),
     path("", include(("app.urls", "app"), namespace="app")),
-    path("", include(("app.bot_api.urls", "app"), namespace="bot_api")),
+    path("bot/", include(("app.bot_api.urls", "app"), namespace="bot_api")),
 ]
 
 if settings.DEBUG:
